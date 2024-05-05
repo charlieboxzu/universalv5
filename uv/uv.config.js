8e49ac1833e3edf5c2e57373b31e1469a8fa1d2e
Nobodycares-lo <140128868+Nobodycares-lo@users.noreply.github.com>
2024-05-05 16:59:49 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,18 +1,3 @@
-const shouldBlock = (req) => req.headers['X-Bare-Url'].includes('porn');
-
-server.on('request', (req, res) => {
-    if (shouldBlock(req)) return res.end('imagine searching up porn. get blocked bozo');
-
-    if (bareServer.shouldRoute(req)) bareServer.routeRequest(req, res);
-    else app(req, res); // Assuming you're using the generic bare + express use case
-});
-
-server.on('upgrade', (req, socket, head) => {
-    if (shouldBlock(req)) return socket.end();
-
-    if (bareServer.shouldRoute(req)) bareServer.routeUpgrade(req, socket, head);
-    else socket.end();
-});
 
 self.__uv$config = {
     prefix: '/service/',
