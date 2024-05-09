0abde38149e5be6377ef66ad60bc30163dd9c261
Nobodycares-lo <140128868+Nobodycares-lo@users.noreply.github.com>
2024-05-09 16:02:52 -0600

    Update register-sw.js

--- assets/register-sw.js
+++ assets/register-sw.js
@@ -1,6 +1,6 @@
 // The service worker registration script
 // This must run successfully before Ultraviolet is available to use
-const BARE_SERVER = "https://tomp.app"
+const BARE_SERVER = "https://bare-hvsxb7l2ca-uc.a.run.app/bare/"
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./uv-sw.js', {
